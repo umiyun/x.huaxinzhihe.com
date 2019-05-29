0b6d91800a3bf493fc33dce1000d9a41f79a8f17
guoqiang <905584560@qq.com>
2019-05-29 16:51:14 +0800

    活动宝报名模块

---
+++ addons/umiacp_apply/core/loader.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: 7³
+ * Date: 2018/7/30 0030
+ * Time: 12:18
+ */
+
+if (!defined('IN_IA')) {
+    exit('Access Denied');
+}
+
+require_once IA_ROOT . '/addons/umiacp_apply/core/defines.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/common.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/setting.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/logs.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/puv.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/lbs.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/juhe.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/sendSms.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/tables.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/refund.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/wxapp.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/wxpay.php';
+require_once IA_ROOT . '/addons/umiacp_apply/core/functions/order.php';
+
